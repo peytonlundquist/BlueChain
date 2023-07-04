e8649c02f817b09da4199a67fb29a3e4391f97d3
Jett Black <jettblack@Jetts-MacBook-Pro.local>
2023-07-04 11:29:32 -0600

    working web app

--- startNetwork.sh
+++ startNetwork.sh
@@ -1,2 +1,3 @@
-mvn clean install ; rm src/main/java/node/nodeRegistry/*.txt ; java -cp target/network-1.0-SNAPSHOT.jar NetworkLauncher
+mvn clean install ; rm src/main/java/node/nodeRegistry/*.txt ; java -cp target/network-1.0-SNAPSHOT.jar NetworkLauncher;
+
 
