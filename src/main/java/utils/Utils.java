81189f3c0d26076ecfaccff9816b2e040835b487
peytonlundquist <peytonlundquist@u.boisestate.edu>
2023-12-08 11:37:14 -0700

    Added deriveQ to Utils

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -2,11 +2,13 @@ package utils;
 
 import static utils.Hashing.getBlockHash;
 
+import java.math.BigInteger;
 import java.security.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 import blockchain.Block;
@@ -131,4 +133,30 @@ public class Utils {
         }
     }
 
+    public static ArrayList<Address> deriveQuorum(Block block, int nonce, Config configValues, ArrayList<Address> globalPeers){
+        String blockHash;
+        if(block != null && block.getPrevBlockHash() != null){
+            try {
+                ArrayList<Address> quorum = new ArrayList<>(); // New list for returning a quorum, list of addr
+                blockHash = Hashing.getBlockHash(block, nonce); // gets the hash of the block
+                BigInteger bigInt = new BigInteger(blockHash, 16); // Converts the hex hash in to a big Int
+                bigInt = bigInt.mod(BigInteger.valueOf(configValues.getNumNodes())); // we mod the big int I guess
+                int seed = bigInt.intValue(); // This makes our seed
+                Random random = new Random(seed); // Makes our random in theory the same across all healthy nodes
+                int quorumNodeIndex; // The index from our global peers from which we select nodes to participate in next quorum
+                Address quorumNode; // The address of thenode from the quorumNode Index to go in to the quorum
+                while(quorum.size() < configValues.getQuorumSize()){
+                    quorumNodeIndex = random.nextInt(configValues.getNumNodes()); // may be wrong but should still work
+                    quorumNode = globalPeers.get(quorumNodeIndex);
+                    if(!containsAddress(quorum, quorumNode)){
+                        quorum.add(globalPeers.get(quorumNodeIndex));
+                    }
+                }
+                return quorum;
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return null;
+    }
 }
